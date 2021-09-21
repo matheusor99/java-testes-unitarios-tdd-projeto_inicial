b6be171394fc3e1b32fa644cac77949ba6064941
matheusor99 <matheusor99@gmail.com>
2021-09-21 17:33:31 -0300

    primeiro teste unitário

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,36 @@
+package br.com.alura.tdd.service;
+
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+class BonusServiceTest {
+
+    @Test
+    public void bonusDeveriaSerZeroParaFuncionarioComSalarioMuiotoAlto() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus((new Funcionario("Matheus", LocalDate.now(), new BigDecimal("25000"))));
+
+        assertEquals(BigDecimal.ZERO, bonus);
+    }
+
+    @Test
+    public void bonusDeveriaSer10PorCentoDoSalario() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus((new Funcionario("Matheus", LocalDate.now(), new BigDecimal("2500"))));
+
+        assertEquals(new BigDecimal("250.0"), bonus);
+    }
+
+    @Test
+    public void bonusDeveriaSer10PorCentoParaSalarioDeExatamenteDezMil() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus((new Funcionario("Matheus", LocalDate.now(), new BigDecimal("10000"))));
+
+        assertEquals(new BigDecimal("1000.0"), bonus);
+    }
+}
